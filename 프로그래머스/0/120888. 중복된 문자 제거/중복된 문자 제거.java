182115ce83c45f6c6f03b8bd1c7bdd86f91bf9a0
권오빈 <jamesobin@naver.com>
2025-03-17 17:12:31 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.95 ms, Memory: 96.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+
+        for(int i = 0; i < my_string.length(); i++) {
+            String c = Character.toString(my_string.charAt(i));
+
+            if(!answer.contains(c)) {
+                answer += c;
+            }
+        }
+
+        return answer;
+    }
+}

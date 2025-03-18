e292d5601fd35801f9c04e1243e7ec794f2c5128
권오빈 <jamesobin@naver.com>
2025-03-18 15:02:05 +0900

    [level 0] Title: 7의 개수, Time: 7.56 ms, Memory: 93.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] array) {
+        int count = 0;
+
+        for(int i = 0; i < array.length; i++) {
+            String snum = array[i] + "";
+
+            for(int j = 0; j < snum.length(); j++) {
+                char c = snum.charAt(j);
+                if(c == '7') {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}

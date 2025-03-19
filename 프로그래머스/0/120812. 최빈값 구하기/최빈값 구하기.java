f1493107514c8ef455fbf99733b367fd889510a5
권오빈 <jamesobin@naver.com>
2025-03-19 17:20:37 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.20 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int solution(int[] array) {
+        int[] countList = new int[array.length];
+
+        for(int i = 0; i < array.length; i++) {
+            int count = 1;
+            for(int j = i + 1; j < array.length; j++) {
+                if(array[i] == array[j]) {
+                    count++;
+                }
+            }
+            countList[i] = count;
+        }
+
+        int max = countList[0];
+        for(int i = 1; i < countList.length; i++) {
+            if(max < countList[i]) {
+                max = countList[i];
+            }
+        }
+
+        int count = 0;
+        for(int i = 0; i < countList.length; i++) {
+            if(countList[i] == max) {
+                count++;
+            }
+        }
+
+        int answer = 0;
+
+        if(count == 1) {
+            for(int i = 0; i < countList.length; i++) {
+                if(countList[i] == max) {
+                    answer = array[i];
+                }
+            }
+        } else {
+            answer = -1;
+        }
+
+        return answer;
+    }
+}

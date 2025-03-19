eb7b044abd4b47879e57e9c21ca96c89e916668b
권오빈 <jamesobin@naver.com>
2025-03-19 14:33:46 +0900

    [level 0] Title: 문자열 밀기, Time: 12.99 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        String checkString = A + A;
+        
+        if(checkString.contains(B)) {
+            while(!A.equals(B)) {
+                A = A.charAt(A.length()-1) + A.substring(0, A.length()-1);
+                answer ++;
+            }
+        } else {
+            answer = -1;
+        }
+
+        return answer;
+    }
+}

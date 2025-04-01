765b612bcb8eeb83519d7a2afdc5f33915caf26b
권오빈 <jamesobin@naver.com>
2025-04-01 10:49:09 +0900

    [level 0] Title: k의 개수, Time: 23.24 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,18 +1,18 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int answer = 0;
-        String str = "";
-        for(int x = i; x <= j; x++) {
-            str += String.valueOf(x);
-        }
+        int count = 0;
 
-        for(int x = 0; x < str.length(); x++) {
-            char c = str.charAt(x);
-            if(c == Character.forDigit(k, 10)) {
-                answer++;
+        for(int x = i; x <= j; x++) {
+            String numString = String.valueOf(x);
+            for(int y = 0; y < numString.length(); y++) {
+                char c = numString.charAt(y);
+
+                if(c == Character.forDigit(k, 10)) {
+                    count++;
+                }
             }
         }
         
-        return answer;
+        return count;
     }
 }

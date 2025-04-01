675f56c1ade5f02c30085ca9d6d0af5689804421
권오빈 <jamesobin@naver.com>
2025-04-01 11:04:50 +0900

    [level 0] Title: 암호 해독, Time: 13.36 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -2,12 +2,17 @@ class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
         
-        for(int i = 0; i < cipher.length(); i++) {
-            char c = cipher.charAt(i);
+//         for(int i = 0; i < cipher.length(); i++) {
+//             char c = cipher.charAt(i);
             
-            if((i+1) % code == 0) {
-                answer += c;
-            }
+//             if((i+1) % code == 0) {
+//                 answer += c;
+//             }
+//         }
+        
+        for(int i = code-1; i < cipher.length(); i += code) {
+            char c = cipher.charAt(i);
+            answer += c;
         }
         return answer;
     }

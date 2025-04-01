157de8864f8ef7b049bd4ecd40b276ff39d42097
권오빈 <jamesobin@naver.com>
2025-04-01 14:47:06 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,23 +1,31 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        String answer = "";
+//         String answer = "";
         
-        for(int i = 0; i < num1; i ++) {
-            answer += my_string.charAt(i);
-        }
+//         for(int i = 0; i < num1; i ++) {
+//             answer += my_string.charAt(i);
+//         }
         
-        answer += my_string.charAt(num2);
+//         answer += my_string.charAt(num2);
         
-        for(int i = (num1+1); i < num2; i++) {
-            answer += my_string.charAt(i);
-        }
+//         for(int i = (num1+1); i < num2; i++) {
+//             answer += my_string.charAt(i);
+//         }
         
-        answer += my_string.charAt(num1);
+//         answer += my_string.charAt(num1);
         
-        for(int i = (num2+1); i < my_string.length(); i++) {
-            answer += my_string.charAt(i);
-        }
+//         for(int i = (num2+1); i < my_string.length(); i++) {
+//             answer += my_string.charAt(i);
+//         }
         
-        return answer;
+//         return answer;
+        
+        char[] charArray = my_string.toCharArray();
+        
+        char temp = charArray[num1];
+        charArray[num1] = charArray[num2];
+        charArray[num2] = temp;
+        
+        return new String(charArray);
     }
 }

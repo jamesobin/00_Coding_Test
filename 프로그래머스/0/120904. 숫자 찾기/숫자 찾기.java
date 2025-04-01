9fb3569d6a882f432d6d59aa692c563d600a9fa8
권오빈 <jamesobin@naver.com>
2025-04-01 11:11:41 +0900

    [level 0] Title: 숫자 찾기, Time: 0.06 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -2,12 +2,21 @@ class Solution {
     public int solution(int num, int k) {
         int answer = -1;
 
-        String snum = "" + num;
-        String[] slist = snum.split("");
+//         String snum = "" + num;
+//         String[] slist = snum.split("");
 
-        for(int i = 0; i < slist.length; i++) {
-            int index = Integer.parseInt(slist[i]);
-            if(index == k) {
+//         for(int i = 0; i < slist.length; i++) {
+//             int index = Integer.parseInt(slist[i]);
+//             if(index == k) {
+//                 answer = i + 1;
+//                 break;
+//             }
+//         }
+        String snum = String.valueOf(num);
+        for(int i = 0; i < snum.length(); i++) {
+            char c = snum.charAt(i);
+            
+            if(c == Character.forDigit(k, 10)) {
                 answer = i + 1;
                 break;
             }

cfb24c015b7e488606836ad8057fec80e83e537e
권오빈 <jamesobin@naver.com>
2025-04-11 11:38:43 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,11 @@
+-- 코드를 작성해주세요
+
+SELECT 
+    DISTINCT D.ID
+    , D.EMAIL
+    , D.FIRST_NAME
+    , D.LAST_NAME
+FROM `DEVELOPERS` AS `D`
+JOIN `SKILLCODES` AS `S`
+ON (D.SKILL_CODE & S.CODE) = S.CODE AND  S.NAME IN ('Python', 'C#')
+ORDER BY D.ID;

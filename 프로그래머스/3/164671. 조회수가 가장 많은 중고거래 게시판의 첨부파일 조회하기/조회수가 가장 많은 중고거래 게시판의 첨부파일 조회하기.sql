38d56f29cd28b778c296e4e909162d740e8af2a3
권오빈 <jamesobin@naver.com>
2025-04-11 16:53:44 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,17 @@
+-- 코드를 입력하세요
+SELECT 
+    CONCAT(
+        '/home/grep/src/'
+        , FILE.BOARD_ID, '/'
+        , FILE.FILE_ID
+        , FILE.FILE_NAME
+        , FILE.FILE_EXT) AS `FILE_PATH`
+FROM `USED_GOODS_BOARD` AS `BOARD`
+JOIN `USED_GOODS_FILE` AS `FILE`
+ON BOARD.BOARD_ID = FILE.BOARD_ID
+WHERE 
+    BOARD.VIEWS IN (
+        SELECT MAX(VIEWS)
+        FROM `USED_GOODS_BOARD`
+    )
+ORDER BY FILE.FILE_ID DESC;

27e7fcaa87e3cb10a4943759445716289c843e2a
권오빈 <jamesobin@naver.com>
2025-08-18 13:12:06 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.go
@@ -0,0 +1,11 @@
+func solution(n int) int {
+    var result int = 0;
+        
+    if n % 7 == 0 {
+        result = n / 7;
+    } else {
+        result = n / 7 + 1;
+    }
+
+    return result
+}

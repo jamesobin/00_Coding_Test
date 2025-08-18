de59fa82b7f83b7d1778055e948764f72ac35d4a
권오빈 <jamesobin@naver.com>
2025-08-18 14:25:55 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 3.55 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.go
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.go
@@ -1,6 +1,6 @@
 func solution(n int, k int) int {
-    var service int = n / 10;
-    var answer int = n * 12000 + (k - service) * 2000;
+    var service int = n / 10
+    var answer int = n * 12000 + (k - service) * 2000
     
-    return answer;
+    return answer
 }
